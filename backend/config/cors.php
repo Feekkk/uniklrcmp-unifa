f79da1255b680514426c682cf47f973f44cebb9f
Wan Afiq <zxduke350@gmail.com>
2025-10-28 00:02:30 +0800

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,7 +3,13 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => [
+        'http://localhost:8080', 
+        'http://localhost:5173', 
+        'http://localhost:3000',
+        'https://unifa.rcmp.edu.my',
+        'https://api.unifa.rcmp.edu.my'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
